b57c08f9852eb18f1356cfb2c6ab5da1ba51d294
James Howell <james.howell@aia.co.uk>
2017-10-04 21:57:12 +0100

    Added supporting HTML

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,23 @@
 import getCurrencyRates from './modules/currency-rates';
 
 document.querySelectorAll("[data-currency-conversion]").forEach(function(form){
+
 	form.addEventListener("submit", function(e){
 
-		var
-		initialCurrencyAmount = this.querySelector("[data-initial-currency]").value,
-		initialCurrencyType = this.querySelector("[data-initial-currency-type]").value,
-		convertedCurrencyAmount = this.querySelector("[data-converted-currency]").value,
-		convertedCurrencyType = this.querySelector("[data-converted-currency-type]").value,
-		convertedCurrencyResult;
+		const initialCurrencyAmount = this.querySelector("[data-initial-currency]").value;
+		const initialCurrencyType = this.querySelector("[data-initial-currency-type]").value;
+		const convertedCurrencyAmount = this.querySelector("[data-converted-currency]").value;
+		const convertedCurrencyType = this.querySelector("[data-converted-currency-type]").value;
+		let convertedCurrencyResult = 0;
 
-		
+		if (initialCurrencyType !== convertedCurrencyType){
 
-		convertedCurrencyResult = getCurrencyRates({
-			initialCurrencyAmount: initialCurrencyAmount,
-			initialCurrencyType: initialCurrencyType,
-			convertedCurrencyAmount: convertedCurrencyAmount,
-			convertedCurrencyType: convertedCurrencyType
-		}).then(function(data){
-			console.log(data);
-
-			if (initialCurrencyType === convertedCurrencyType){
-				return initialCurrencyAmount;
-			}
-			else{
+			convertedCurrencyResult = getCurrencyRates({
+				initialCurrencyAmount: initialCurrencyAmount,
+				initialCurrencyType: initialCurrencyType,
+				convertedCurrencyAmount: convertedCurrencyAmount,
+				convertedCurrencyType: convertedCurrencyType
+			}).then(function(data){
 
 				const poundsInDollars = 1 / data.usdGBP;
 				const bitcoinsInDollars = initialCurrencyAmount * data.bitcoinUSD;
@@ -45,13 +39,33 @@ document.querySelectorAll("[data-currency-conversion]").forEach(function(form){
 
 				}
 
-			}
-		}).then(function(convertedCurrencyTotal){
-			document.querySelector("[data-converted-currency]").value = convertedCurrencyTotal;
-		});
+			}).then(function(convertedCurrencyTotal){
+				document.querySelector("[data-converted-currency]").value = convertedCurrencyTotal;
+			});
+
+		} else{
+			document.querySelector("[data-converted-currency]").value = initialCurrencyAmount;
+		}
 
 
 
 		e.preventDefault();
+	});
+
+	form.querySelectorAll("[data-linked-select]").forEach(function(selectBox){
+
+		selectBox.addEventListener("change", function(){
+			let selectID = this.attributes.id.value;
+
+			form.querySelectorAll('[data-linked-to="'+selectID+'"]').forEach((element)=>{
+				element.innerText = 'Amount in '+this.value;
+
+				if (element.hasAttribute("placeholder")) {
+					element.attributes.placeholder.value = 'Amount in '+this.value;
+				}
+
+			});
+		});
+
 	});
 });
--- src/js/modules/currency-rates.js
+++ src/js/modules/currency-rates.js
@@ -2,27 +2,36 @@ export default function getCurrencyRates(conversionData) {
 
 	// Check if either currency is BitCoin
 
-	if ( (conversionData.initialCurrencyType === "XBT" ||  conversionData.convertedCurrencyType === "XBT") &&  conversionData.initialCurrencyType !== conversionData.convertedCurrencyType ) {
+	if ( (conversionData.initialCurrencyType === "XBT" ||  conversionData.convertedCurrencyType === "XBT")) {
 
 
-		var bitcoinUSD = fetch('https://api.coindesk.com/v1/bpi/currentprice/usd.json').then(function(response){ 
+		// Use CoinDesk API to retrieve the current Bitcoin to US Dollar exchange rate
+		
+		const bitcoinUSD = fetch('https://api.coindesk.com/v1/bpi/currentprice/usd.json').then(function(response){ 
 			return response.json()
 		}).then(function(data){
 			return data.bpi.USD.rate_float;
 		});
 	
 
-		var usdGBP = fetch('https://openexchangerates.org/api/latest.json?app_id=00b59e9c79ba43c887c43fb006ba1d07').then(function(response){
+		// Use OpenExchangeRates API to retrieve the current US Dollar to Great British Pound exchange rate
+
+		const usdGBP = fetch('https://openexchangerates.org/api/latest.json?app_id=00b59e9c79ba43c887c43fb006ba1d07').then(function(response){
 			return response.json()
 		}).then(function(data){
 			return data.rates.GBP;
 		});
 	
 
-		var combinedData = {"bitcoinUSD":{},"usdGBP":{}};
+		// Create a blank object with named attributes to store the exchange rates in
+
+		const combinedData = {"bitcoinUSD":{},"usdGBP":{}};
 	
 
-		var totalData = Promise.all([bitcoinUSD,usdGBP]).then(function(values){
+
+		// Combine the two Fetch API requests into a single promise. The 'then' function which runs when the promise is resolved will only run if both APIs gave a response.
+
+		const totalData = Promise.all([bitcoinUSD,usdGBP]).then(function(values){
 			combinedData["bitcoinUSD"] = values[0];
 			combinedData["usdGBP"] = values[1];
 
